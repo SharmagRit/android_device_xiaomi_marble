bdde4ca51c564ed1e8e382affb1de4df4cba8a2f
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-11-28 02:07:27 +0000

    marble: Disable oem unlock
    * Fixes safetynet
    
    Change-Id: I93e5e13fc47551f153533118d68a6b7fbc7f1600

--- libinit/init_xiaomi_marble.cpp
+++ libinit/init_xiaomi_marble.cpp
@@ -62,4 +62,5 @@ void vendor_load_properties() {
 
     // SafetyNet workaround
     property_override("ro.boot.verifiedbootstate", "green");
+    property_override("ro.oem_unlock_supported", "0");
 }
